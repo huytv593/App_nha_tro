9c7ceb65e72184cb936a620926f2cba746b4cbe2
huytv <huyhuong>
2015-04-15 00:24:22 +0700

    add volley libs

--- app/src/main/java/bdnt/example/com/bandonhatro/MapFragment.java
+++ app/src/main/java/bdnt/example/com/bandonhatro/MapFragment.java
@@ -1,12 +1,13 @@
 package bdnt.example.com.bandonhatro;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class MapFragment extends Fragment {
-
+    public  static View view;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +16,17 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_map, container, false);
+        if (view != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+                parent.removeView(view);
+        }
+        try {
+            view = inflater.inflate(R.layout.fragment_map, container, false);
+        } catch (InflateException e) {
+    /* map is already there, just return view as it is */
+        }
+        return view;
 
-        return v;
     }
 }
