872ab94f2355f999fa11d4333c56e75400815626
huytv <huyhuong>
2015-04-15 08:32:21 +0700

    add volley libs

---
+++ app/src/main/java/bdnt/example/com/bandonhatro/AppConfig.java
@@ -0,0 +1,10 @@
+package bdnt.example.com.bandonhatro;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.40/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.40/android_login_api/include";
+    public static String REGISTER="register";
+}
--- app/src/main/java/bdnt/example/com/bandonhatro/OptionFragment.java
+++ app/src/main/java/bdnt/example/com/bandonhatro/OptionFragment.java
@@ -1,6 +1,7 @@
 package bdnt.example.com.bandonhatro;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -25,39 +26,47 @@ public class OptionFragment extends Fragment {
         btnDangTin = (Button) v.findViewById(R.id.btnDangTin);
         btnDangTin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Dialog b = new Dialog(getActivity());
-                b.setContentView(R.layout.login);
-                b.setTitle("Đăng nhập");
-                btnLinkDangKi = (Button) b.findViewById(R.id.btnLinkDangKi);
-                btnLinkDangKi.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        final Dialog c = new Dialog(getActivity());
-                        c.setContentView(R.layout.signup);
-                        c.setTitle("Đăng kí");
-                        final Button btnHuyBo = (Button) c.findViewById(R.id.btnHuyBo);
-                        btnHuyBo.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                c.cancel();
-                            }
-                        });
-                        c.show();
-
-
-                    }
-                });
-                b.show();
-            }
-        });
-        btnGioiThieu = (Button) v.findViewById(R.id.btnGioiThieu);
-        btnGioiThieu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),LoginActivity.class);
+                startActivity(intent);
 
             }
         });
+//        btnDangTin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Dialog b = new Dialog(getActivity());
+//                b.setContentView(R.layout.login);
+//                b.setTitle("Đăng nhập");
+//                btnLinkDangKi = (Button) b.findViewById(R.id.btnLinkToRegisterScreen);
+//                btnLinkDangKi.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        final Dialog c = new Dialog(getActivity());
+//                        c.setContentView(R.layout.signup);
+//                        c.setTitle("Đăng kí");
+////                        final Button btnHuyBo = (Button) c.findViewById(R.id.btnHuyBo);
+////                        btnHuyBo.setOnClickListener(new View.OnClickListener() {
+////                            @Override
+////                            public void onClick(View view) {
+////                                c.cancel();
+////                            }
+////                        });
+//                        c.show();
+//
+//
+//                    }
+//                });
+//                b.show();
+//            }
+//        });
+//        btnGioiThieu = (Button) v.findViewById(R.id.btnGioiThieu);
+//        btnGioiThieu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
         return v;
     }
 }
