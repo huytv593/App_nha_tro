46ff4caea71e87a718ec012ea551d34d3ffd198f
huytv <huyhuong>
2015-05-12 15:23:43 +0700

    fix room list view

--- app/src/main/java/bdnt/example/com/bandonhatro/VolleyListView/RoomListViewActivity.java
+++ app/src/main/java/bdnt/example/com/bandonhatro/VolleyListView/RoomListViewActivity.java
@@ -34,7 +34,6 @@ public class RoomListViewActivity extends ActionBarActivity {
     TextView nothing_found, result_title;
     Button change_to_mapView, size_result;
     Map<String, String> params;
-    //String jsonExample="{\"Nhatro\":[{\"id\":\"1\",\"title\":\"Nhà trọ 1\",\"created_by\":\"1\",\"created_at\":\"2015-04-21\",\"end_at\":\"2015-04-29\",\"price\":\"2000\",\"city\":\"Hà Nội\",\"district\":\"Cầu Giấy\",\"precinct\":\"Dịch Vọng Hậu\",\"street\":\"Xuân Thuỷ\",\"address\":\"114 Xuân Thuỷ\",\"area\":\"20\",\"info\":\"nsdasdnasdnsab nbsdnamsn sadas dn\",\"imga\":\"1.jpg\",\"imgb\":\"1.jpg\",\"imgc\":\"1.jpg\",\"imgd\":\"1.jpg\",\"long\":null,\"lat\":null},{\"id\":\"2\",\"title\":\"Nhà trọ 2\",\"created_by\":\"1\",\"created_at\":\"2015-04-22\",\"end_at\":\"2015-04-26\",\"price\":\"3000\",\"city\":\"Hà Nội\",\"district\":\"Cầu Giấy\",\"precinct\":\"Mai Dịch\",\"street\":\"Hồ Tùng Mậu\",\"address\":\"14\",\"area\":\"26\",\"info\":\"đâsdas\",\"imga\":\"1.jpg\",\"imgb\":\"1.jpg\",\"imgc\":\"1.jpg\",\"imgd\":\"1.jpg\",\"long\":null,\"lat\":null},{\"id\":\"3\",\"title\":\"Nhà trọ 3\",\"created_by\":\"1\",\"created_at\":\"2015-04-22\",\"end_at\":\"2015-04-30\",\"price\":\"2300\",\"city\":\"Hà Nội\",\"district\":\"Cầu Giấy\",\"precinct\":\"Mai Dịch\",\"street\":\"Phạm Văn Đồng\",\"address\":\"115\",\"area\":\"23\",\"info\":\"13213123 đấ \",\"imga\":\"1.jpg\",\"imgb\":\"1.jpg\",\"imgc\":\"1.jpg\",\"imgd\":\"1.jpg\",\"long\":null,\"lat\":null},{\"id\":\"4\",\"title\":\"Nhà trọ 4\",\"created_by\":\"1\",\"created_at\":\"2015-04-22\",\"end_at\":\"2015-04-30\",\"price\":\"3200\",\"city\":\"Hà Nội\",\"district\":\"Cầu Giấy\",\"precinct\":\"Mai Dịch\",\"street\":\"Doãn Kế Thiện\",\"address\":\"30\",\"area\":\"30\",\"info\":\"DKLSANDSLAD\",\"imga\":\"1.jpg\",\"imgb\":\"1.jpg\",\"imgc\":\"1.jpg\",\"imgd\":\"1.jpg\",\"long\":null,\"lat\":null},{\"id\":\"5\",\"title\":\"Nhà trọ 5\",\"created_by\":\"1\",\"created_at\":\"2015-04-22\",\"end_at\":\"2015-08-25\",\"price\":\"2000\",\"city\":\"Hà Nội\",\"district\":\"Cầu Giấy\",\"precinct\":\"Dịch Vọng \",\"street\":\"Thành Thái\",\"address\":\"128\",\"area\":\"20\",\"info\":\"dasdsad asd \",\"imga\":\"1.jpg\",\"imgb\":\"1.jpg\",\"imgc\":\"1.jpg\",\"imgd\":\"1.jpg\",\"long\":null,\"lat\":null},{\"id\":\"6\",\"title\":\"Nhà trọ 6\",\"created_by\":\"1\",\"created_at\":\"2015-04-22\",\"end_at\":\"2015-08-30\",\"price\":\"2500\",\"city\":\"Hà Nội\",\"district\":\"Ba Đình\",\"precinct\":\"Đội Cấn \",\"street\":\"Ngọc Hà\",\"address\":\"117\",\"area\":\"20\",\"info\":\"asdsa \",\"imga\":\"1.jpg\",\"imgb\":\"1.jpg\",\"imgc\":\"1.jpg\",\"imgd\":\"1.jpg\",\"long\":null,\"lat\":null},{\"id\":\"7\",\"title\":\"Nhà trọ 7\",\"created_by\":\"1\",\"created_at\":\"2015-04-22\",\"end_at\":\"2015-08-27\",\"price\":\"2400\",\"city\":\"Hà Nội\",\"district\":\"Đống Đa\",\"precinct\":\"Láng Thượng\",\"street\":\"Pháo Đài Láng\",\"address\":\"23\",\"area\":\"20\",\"info\":\"dksnladnsald\",\"imga\":\"1.jpg\",\"imgb\":\"1.jpg\",\"imgc\":\"1.jpg\",\"imgd\":\"1.jpg\",\"long\":null,\"lat\":null},{\"id\":\"8\",\"title\":\"Nhà trọ 8\",\"created_by\":\"1\",\"created_at\":\"2015-04-22\",\"end_at\":\"2015-08-29\",\"price\":\"2400\",\"city\":\"Hà Nội\",\"district\":\"Đống Đa\",\"precinct\":\"Láng Hạ\",\"street\":\"Huỳnh Thúc Kháng\",\"address\":\"132\",\"area\":\"20\",\"info\":\"jdsald.jpg\",\"imga\":\"1.jpg\",\"imgb\":\"1.jpg\",\"imgc\":\"1.jpg\",\"imgd\":\"1.jpg\",\"long\":null,\"lat\":null}]}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +49,6 @@ public class RoomListViewActivity extends ActionBarActivity {
         setContentView(R.layout.activity_room_list_view);
 
         params = (Map) getIntent().getSerializableExtra("params");
-        Log.d("result screen params", Integer.toString(params.size()));
 
         listView = (ListView) findViewById(R.id.roomList);
 
@@ -61,7 +59,6 @@ public class RoomListViewActivity extends ActionBarActivity {
 
         pDialog = new ProgressDialog(this);
         pDialog.setMessage("Loading...");
-//        pDialog.setCancelable(false);
         pDialog.show();
 
     }
@@ -71,17 +68,7 @@ public class RoomListViewActivity extends ActionBarActivity {
             @Override
             public void onResponse(String response) {
                 pDialog.hide();
-                Toast.makeText(RoomListViewActivity.this, response, Toast.LENGTH_SHORT).show();
-//                Gson gson = new Gson();
-//
-//                JsonParser jsonParser= new JsonParser();
-//                JsonArray roomArray= jsonParser.parse(response).getAsJsonArray();
                 roomList = new ArrayList<>();
-//                for (JsonElement aRoom: roomArray){
-//                    Room room = gson.fromJson(aRoom,Room.class);
-//                    roomList.add(room);
-//                }
-//                Log.d("Test parse json array",roomList.get(0).toString());
                 try {
                     JSONObject jsonObject = new JSONObject(response);
                     JSONArray jsonArray = jsonObject.getJSONArray("Nhatro");
@@ -125,8 +112,6 @@ public class RoomListViewActivity extends ActionBarActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-//                Toast.makeText(RoomListViewActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-//                 Log.d("ssss", error.getMessage());
             }
         }
         ) {
